d551fc5366ad843ef67adb29a110dad28fd6ef12
Kirill Kolchanov <kirill.kolchanov@onlinesolutionsb2b.com>
2024-02-28 01:22:04 +0500

    (OTUS-HW06): add cloud-init.yaml

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -4,6 +4,5 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongod
 sudo apt update; sudo apt-get install -y mongodb-org && sudo systemctl start mongod
 if [ $? -eq 0 ]; then
 echo "MongoDB was installed" >> /tmp/log_deploy.log; sudo systemctl start mongod
-else "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1;
-fi
-exit 0
+else echo "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+exit 0;
--- install_ruby.sh
+++ install_ruby.sh
@@ -3,11 +3,9 @@ sudo apt update && sudo apt install -y ruby-full ruby-bundler build-essential;
 ruby -v
 if [ $? -eq 0 ]; then
 echo "Ruby was installed" >> /tmp/log_deploy.log
-else "WARN: Ruby wasn't installed" >> /tmp/log_deploy.log exit 1;
-fi
+else echo "WARN: Ruby wasn't installed" >> /tmp/log_deploy.log exit 1; fi
 bundler -v
 if [ $? -eq 0 ]; then
 echo "Bundler was installed" >> /tmp/log_deploy.log
-else "WARN: Bundler wasn't installed" >> /tmp/log_deploy.log exit 1;
-fi
-exit 0
+else echo "WARN: Bundler wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+exit 0;
