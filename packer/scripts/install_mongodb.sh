e76e5a74a9578347109034b09d494a72007c1e7b
Kirill Kolchanov <kirill.kolchanov@onlinesolutionsb2b.com>
2024-03-24 00:46:12 +0500

    fix(OTUS-HW-05): second homework

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-apt update && apt-get install -y mongodb-org && systemctl start mongod
-if [ $? -eq 0 ]; then
-echo "MongoDB was installed" >> /tmp/log_deploy.log; systemctl start mongod; systemctl enable mongod.service
-else echo "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1; fi
-exit 0;
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+apt-get update; sleep 5;
+apt-get install -y mongodb-org; sleep 3;
+systemctl enable mongod.service
+# if [ $? -eq 0 ]; then
+# echo "MongoDB was installed" >> /tmp/log_deploy.log; systemctl start mongod; systemctl enable mongod.service
+# else echo "WARN: MongoDB wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+# exit 0;
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-apt update && apt install -y ruby-full ruby-bundler build-essential;
-ruby -v
-if [ $? -eq 0 ]; then
-echo "Ruby was installed" >> /tmp/log_deploy.log
-else echo "WARN: Ruby wasn't installed" >> /tmp/log_deploy.log exit 1; fi
-bundler -v
-if [ $? -eq 0 ]; then
-echo "Bundler was installed" >> /tmp/log_deploy.log
-else echo "WARN: Bundler wasn't installed" >> /tmp/log_deploy.log exit 1; fi
-exit 0;
+
+# apt-get install aptitude -y; sleep 2; /usr/sbin/ldconfig; sleep 2
+apt-get update; sleep 5;
+apt-get install -y fontconfig-config fonts-dejavu-core libfontconfig1 libgmp-dev libgmpxx4ldbl libtcl8.6 libtcltk-ruby libtk8.6 libxft2 libxrender1 libxss1 ri ruby-dev ruby-full
+  ruby2.3-dev ruby2.3-doc ruby2.3-tcltk x11-common; sleep 5;
+apt-get install -y ruby-bundler; sleep 5;
+apt-get install -y build-essential; sleep 5
+# /usr/bin/ruby -v
+# if [ $? -eq 0 ]; then
+# echo "Ruby was installed" >> /tmp/log_deploy.log
+# else echo "WARN: Ruby wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+# /usr/bin/bundler -v
+# if [ $? -eq 0 ]; then
+# echo "Bundler was installed" >> /tmp/log_deploy.log
+# else echo "WARN: Bundler wasn't installed" >> /tmp/log_deploy.log exit 1; fi
+# exit 0;
