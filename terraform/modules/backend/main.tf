b480007e5d6c26cdea57aa30ddd162824289fb1d
Kirill Kolchanov <k.kolchanov@gmail.com>
2024-07-23 23:23:40 +0500

    feat(OTUS-HW-TF-2): add files for module

---
+++ terraform/modules/backend/main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
+
+// Создание сервисного аккаунта
+resource "yandex_iam_service_account" "sa" {
+  name = "s3-admin"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "bucket-test" {
+  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket                = "tfbackend"
+  max_size              = 2097152
+  default_storage_class = "standard"
+  anonymous_access_flags {
+    read        = false
+    list        = true
+    config_read = false
+  }
+  tags = {
+    Name         = "remote-backend"
+    project_name = "OTUS-HW"
+  }
+}
---
+++ terraform/modules/backend/variables.tf
@@ -0,0 +1,44 @@
+variable "cloud_id" {
+  type        = string
+  description = "yandex cloud ID"
+}
+variable "folder_id" {
+  type        = string
+  description = "yandex cloud folder ID"
+}
+variable "zone" {
+  type        = string
+  description = "Default zone"
+  default     = "ru-central1-a"
+}
+# variable "public_key_path" {
+#   type        = string
+#   description = "Full path to public key"
+# }
+# variable "private_key_path" {
+#   type        = string
+#   description = "Full path to private key"
+# }
+# variable "image_id" {
+#   type        = string
+#   description = "yandex cloud image ID"
+# }
+# variable "subnet_id" {
+#   type        = string
+#   description = "yandex cloud subnet id of ru-central1-a"
+# }
+variable "service_account_key_file" {
+  type        = string
+  description = "Full path to json key file"
+}
+# variable "reddit_instance_count" {
+#   type = number
+# }
+# variable "app_disk_image" {
+#   description = "Disk image for reddit app"
+#   default     = "reddit-app-base"
+# }
+# variable "db_disk_image" {
+#   description = "Disk image for reddit db"
+#   default     = "reddit-db-base"
+# }
