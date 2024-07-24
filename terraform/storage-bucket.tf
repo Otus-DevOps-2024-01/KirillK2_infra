2489cbf6e6e21d00980e6fb260710a624412d34e
Kirill Kolchanov <k.kolchanov@gmail.com>
2024-07-25 00:10:32 +0500

    feat(OTUS-HW-TF-2): add variables to storage-bkucket for autotest

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,64 +7,64 @@
 #   required_version = ">= 0.13"
 # }
 
-variable "cloud_id" {
-  type        = string
-  description = "yandex cloud ID"
-}
-variable "folder_id" {
-  type        = string
-  description = "yandex cloud folder ID"
-}
-variable "zone" {
-  type        = string
-  description = "Default zone"
-  default     = "ru-central1-a"
-}
+# variable "cloud_id" {
+#   type        = string
+#   description = "yandex cloud ID"
+# }
+# variable "folder_id" {
+#   type        = string
+#   description = "yandex cloud folder ID"
+# }
+# variable "zone" {
+#   type        = string
+#   description = "Default zone"
+#   default     = "ru-central1-a"
+# }
 
-variable "service_account_key_file" {
-  type        = string
-  description = "Full path to json key file"
-}
+# variable "service_account_key_file" {
+#   type        = string
+#   description = "Full path to json key file"
+# }
 
-provider "yandex" {
-  service_account_key_file = var.service_account_key_file
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
+# provider "yandex" {
+#   service_account_key_file = var.service_account_key_file
+#   cloud_id                 = var.cloud_id
+#   folder_id                = var.folder_id
+#   zone                     = var.zone
+# }
 
 // Создание сервисного аккаунта
-resource "yandex_iam_service_account" "sa" {
-  name = "s3-admin"
-}
+# resource "yandex_iam_service_account" "sa" {
+#   name = "s3-admin"
+# }
 
 // Назначение роли сервисному аккаунту
-resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
-  folder_id = var.folder_id
-  role      = "storage.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
-}
+# resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+#   folder_id = var.folder_id
+#   role      = "storage.admin"
+#   member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+# }
 
 // Создание статического ключа доступа
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
-  description        = "static access key for object storage"
-}
+# resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+#   service_account_id = yandex_iam_service_account.sa.id
+#   description        = "static access key for object storage"
+# }
 
 // Создание бакета с использованием ключа
-resource "yandex_storage_bucket" "bucket-test" {
-  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket                = "tfbackend"
-  max_size              = 2097152
-  default_storage_class = "standard"
-  anonymous_access_flags {
-    read        = false
-    list        = true
-    config_read = false
-  }
-  tags = {
-    Name         = "remote-backend"
-    project_name = "OTUS-HW"
-  }
-}
+# resource "yandex_storage_bucket" "bucket-test" {
+#   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+#   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+#   bucket                = "tfbackend"
+#   max_size              = 2097152
+#   default_storage_class = "standard"
+#   anonymous_access_flags {
+#     read        = false
+#     list        = true
+#     config_read = false
+#   }
+#   tags = {
+#     Name         = "remote-backend"
+#     project_name = "OTUS-HW"
+#   }
+# }
